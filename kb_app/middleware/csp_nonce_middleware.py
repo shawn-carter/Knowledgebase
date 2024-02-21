839e5381a1871bbc3cea33127c84543737b18dab
RCBC-Dev <93768555+RCBC-Dev@users.noreply.github.com>
2024-02-21 18:09:35 +0000

    Added further directives to CSP middleware to deal with style-src, img-src, connect-src, frame-src, font-src, media-src, object-src and manifest-src

--- kb_app/middleware/csp_nonce_middleware.py
+++ kb_app/middleware/csp_nonce_middleware.py
@@ -1,4 +1,3 @@
-import base64
 import os
 from django.utils.deprecation import MiddlewareMixin
 
@@ -9,15 +8,21 @@ class CSPNonceMiddleware(MiddlewareMixin):
         request.csp_nonce = nonce
 
     def process_response(self, request, response):
-        # Add the nonce to the CSP header
+        # Add the nonce and other CSP directives
         if hasattr(request, 'csp_nonce'):
-            csp = ("script-src 'self' 'nonce-{}' "
-                "https://cdn.datatables.net "
-                "https://cdn.jsdelivr.net "
-                "https://cdn.quilljs.com "
-                "https://code.jquery.com; "
+            csp = (
+                "default-src 'self'; "
+                "script-src 'self' 'nonce-{}' https://cdn.datatables.net https://cdn.jsdelivr.net https://cdn.quilljs.com https://code.jquery.com; "
+                "style-src 'self' https://cdn.datatables.net https://cdn.jsdelivr.net https://cdn.quilljs.com 'sha256-aqNNdDLnnrDOnTNdkJpYlAxKVJtLt9CtFLklmInuUAE=' 'sha256-r06yVUBqP+7ZbDWovXc9AqepL8NwsS69BQIUpScMDvU=' 'sha256-0EZqoz+oBhx7gF4nvY2bSqoGyy4zLjNF+SDQXGp/ZrY=' 'sha256-RvAvREUHojDuwHylTVWZp9DhleqLs6ml8G7LpjCF+EY=' 'sha256-ZdHxw9eWtnxUb3mk6tBS+gIiVUPE3pGM470keHPDFlE='; "
+                "img-src 'self' https://cdn.jsdelivr.net; "
+                "connect-src 'self'; "
+                "frame-src 'self'; "
+                "font-src 'self'; "
+                "media-src 'self'; "
+                "object-src 'none'; "
+                "manifest-src 'self'; "
                 "frame-ancestors 'self'; "  # Restrict framing of your site to same origin
                 "form-action 'self';"  # Restrict form submission to same origin
-                ).format(request.csp_nonce)
+            ).format(request.csp_nonce)
             response['Content-Security-Policy'] = csp
         return response

2d8d15e6c18e96718f320c50fb24ad36805f8b5e
RCBC-Dev <93768555+RCBC-Dev@users.noreply.github.com>
2024-02-21 17:48:35 +0000

    Added frame-ancestors and form-action to Content Security Policy

--- kb_app/middleware/csp_nonce_middleware.py
+++ kb_app/middleware/csp_nonce_middleware.py
@@ -11,6 +11,13 @@ class CSPNonceMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         # Add the nonce to the CSP header
         if hasattr(request, 'csp_nonce'):
-            csp = "script-src 'self' 'nonce-{}' https://cdn.datatables.net https://cdn.jsdelivr.net https://cdn.quilljs.com https://code.jquery.com;".format(request.csp_nonce)
+            csp = ("script-src 'self' 'nonce-{}' "
+                "https://cdn.datatables.net "
+                "https://cdn.jsdelivr.net "
+                "https://cdn.quilljs.com "
+                "https://code.jquery.com; "
+                "frame-ancestors 'self'; "  # Restrict framing of your site to same origin
+                "form-action 'self';"  # Restrict form submission to same origin
+                ).format(request.csp_nonce)
             response['Content-Security-Policy'] = csp
         return response
--- kb_project/settings.py
+++ kb_project/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'kb_app.middleware.csp_nonce_middleware.CSPNonceMiddleware',
-    'csp.middleware.CSPMiddleware',
+    #'csp.middleware.CSPMiddleware',
 ]
 
 ROOT_URLCONF = 'kb_project.urls'
@@ -81,39 +81,27 @@ TEMPLATES = [
 WSGI_APPLICATION = 'kb_project.wsgi.application'
 
 # Content Security Policy (CSP)
-CSP_DEFAULT_SRC = ("'self'", )
-CSP_SCRIPT_SRC = (
-    "'self'","'nonce-{}'"
-    'https://cdn.datatables.net', 
-    'https://cdn.jsdelivr.net', 
-    'https://cdn.quilljs.com', 
-    'https://code.jquery.com',
-    "'sha256-aqNNdDLnnrDOnTNdkJpYlAxKVJtLt9CtFLklmInuUAE='",
-    "'sha256-BVHXCPA4OFXCfWS+ZsY5hIuf3D8+7LFZGKL5IRkmHFU='",
-    "'sha256-BlytFmIIb51t6hGjjd9/VhTerovSuN7JfGMToS0WfYQ='",
-    "'sha256-CdPEdf7NiQ6K295E0TLwoOcryfqgXeFlzOYyBpX67Ig='",
-    "'sha256-dn/d/mxDWHH7jwOB+nxnivi9k+pO//eoyYHCjBYKc/A='",
-    # ... (any other script sources you need) ...
-)
-CSP_STYLE_SRC = (
-    "'self'",
-    'https://cdn.datatables.net', 
-    'https://cdn.jsdelivr.net',
-    'https://cdn.quilljs.com',
-    "'sha256-aqNNdDLnnrDOnTNdkJpYlAxKVJtLt9CtFLklmInuUAE='",
-    "'sha256-r06yVUBqP+7ZbDWovXc9AqepL8NwsS69BQIUpScMDvU='",
-    "'sha256-0EZqoz+oBhx7gF4nvY2bSqoGyy4zLjNF+SDQXGp/ZrY='",
-    "'sha256-RvAvREUHojDuwHylTVWZp9DhleqLs6ml8G7LpjCF+EY='",
-    "'sha256-ZdHxw9eWtnxUb3mk6tBS+gIiVUPE3pGM470keHPDFlE='",
+# CSP_DEFAULT_SRC = ("'self'", )
+
+# CSP_STYLE_SRC = (
+#     "'self'",
+#     'https://cdn.datatables.net', 
+#     'https://cdn.jsdelivr.net',
+#     'https://cdn.quilljs.com',
+#     "'sha256-aqNNdDLnnrDOnTNdkJpYlAxKVJtLt9CtFLklmInuUAE='",
+#     "'sha256-r06yVUBqP+7ZbDWovXc9AqepL8NwsS69BQIUpScMDvU='",
+#     "'sha256-0EZqoz+oBhx7gF4nvY2bSqoGyy4zLjNF+SDQXGp/ZrY='",
+#     "'sha256-RvAvREUHojDuwHylTVWZp9DhleqLs6ml8G7LpjCF+EY='",
+#     "'sha256-ZdHxw9eWtnxUb3mk6tBS+gIiVUPE3pGM470keHPDFlE='",
          
-    # ... (any other style sources you need) ...
-)
-CSP_IMG_SRC = (
-    "'self'",
-    'https://cdn.jsdelivr.net',
-    'http://www.w3.org/2000/svg',
-    # ... (any other image sources you need) ...
-)
+#     # ... (any other style sources you need) ...
+# )
+# CSP_IMG_SRC = (
+#     "'self'",
+#     'https://cdn.jsdelivr.net',
+#     'http://www.w3.org/2000/svg',
+#     # ... (any other image sources you need) ...
+# )
 
 
 # Check if running in Azure environment

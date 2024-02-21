86d0ddfbf8251aec6529d02b5b43fad6d8f281c0
Shawn Carter <50450963+shawn-carter@users.noreply.github.com>
2024-02-21 13:44:11 +0000

    Still lookingat security settings

--- kb_project/settings.py
+++ kb_project/settings.py
@@ -90,7 +90,7 @@ if os.environ.get('ENVIRONMENT') == 'PRODUCTION':
     # Secure settings for production in Azure
     SESSION_COOKIE_SECURE = True
     SESSION_COOKIE_HTTPONLY = True
-    CSRF_COOKIE_SECURE = True
+    #CSRF_COOKIE_SECURE = True
     #SECURE_BROWSER_XSS_FILTER = True
     #SECURE_CONTENT_TYPE_NOSNIFF = True
     #X_FRAME_OPTIONS = 'DENY'
